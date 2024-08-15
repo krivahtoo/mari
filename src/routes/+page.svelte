1f713faec452db898018f7e7a74dbbca41fc250c
Noah Too <krivahtoo@gmail.com>
2024-08-15 13:59:54 +0300

    switch to sveltekit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,51 @@
+<script>
+  import { invoke } from "@tauri-apps/api/tauri";
+  import * as ContextMenu from "$lib/components/ui/context-menu/index.js";
+
+  let name = "";
+  let greetMsg = "";
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    greetMsg = await invoke("greet", { name });
+  }
+</script>
+
+<ContextMenu.Root>
+  <ContextMenu.Trigger class="flex h-screen w-full items-center justify-center">
+    <div class="container">
+      <h1>Welcome to Tauri!</h1>
+
+      <div class="row">
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" class="logo vite" alt="Vite Logo" />
+        </a>
+        <a href="https://tauri.app" target="_blank">
+          <img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
+        </a>
+        <a href="https://kit.svelte.dev" target="_blank">
+          <img src="/svelte.svg" class="logo svelte-kit" alt="SvelteKit Logo" />
+        </a>
+      </div>
+
+      <p>Click on the Tauri, Vite, and SvelteKit logos to learn more.</p>
+
+      <form class="row" on:submit|preventDefault={greet}>
+        <input
+          id="greet-input"
+          placeholder="Enter a name..."
+          bind:value={name}
+        />
+        <button type="submit">Greet</button>
+      </form>
+
+      <p>{greetMsg}</p>
+    </div>
+  </ContextMenu.Trigger>
+  <ContextMenu.Content>
+    <ContextMenu.Item>Profile</ContextMenu.Item>
+    <ContextMenu.Item>Billing</ContextMenu.Item>
+    <ContextMenu.Item>Team</ContextMenu.Item>
+    <ContextMenu.Item>Subscription</ContextMenu.Item>
+  </ContextMenu.Content>
+</ContextMenu.Root>
--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,21 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { sveltekit } from "@sveltejs/kit/vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte()],
-  // prevent vite from obscuring rust errors
+export default defineConfig(async () => ({
+  plugins: [sveltekit()],
+
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  //
+  // 1. prevent vite from obscuring rust errors
   clearScreen: false,
-  // Tauri expects a fixed port, fail if that port is not available
+  // 2. tauri expects a fixed port, fail if that port is not available
   server: {
+    port: 1420,
     strictPort: true,
+    watch: {
+      // 3. tell vite to ignore watching `src-tauri`
+      ignored: ["**/src-tauri/**"],
+    },
   },
-  // to make use of `TAURI_PLATFORM`, `TAURI_ARCH`, `TAURI_FAMILY`,
-  // `TAURI_PLATFORM_VERSION`, `TAURI_PLATFORM_TYPE` and `TAURI_DEBUG`
-  // env variables
-  envPrefix: ['VITE_', 'TAURI_'],
-  build: {
-    // Tauri supports es2021
-    target: ['es2021', 'chrome100', 'safari13'],
-    // don't minify for debug builds
-    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
-    // produce sourcemaps for debug builds
-    sourcemap: !!process.env.TAURI_DEBUG,
-  },
-})
+}));
